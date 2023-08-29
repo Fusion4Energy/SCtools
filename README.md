371365b9492c51d154f0d553a07cf52cb810e763
Davide Laghi <davide.laghi01@gmail.com>
2023-08-29 10:24:48 +0200

    added load points

--- README.md
+++ README.md
@@ -9,6 +9,11 @@ This is a package grouping a set of useful IronPython routines to manipulate/ana
 
 SCtools contains many independent tools.
 
+## Index of scripts
+*This section is still a work in progress*
+
+- [`load_csv_points.py`](sctools/load_csv_points.py) is a simple script that allows to read a .csv file containing points coordinates and render them as spheres in SpaceClaim.
+
 ## License
 Copyright 2019 F4E | European Joint Undertaking for ITER and the Development of Fusion Energy (‘Fusion for Energy’). Licensed under the EUPL, Version 1.1 or - as soon they will be approved by the European Commission - subsequent versions of the EUPL (the “Licence”). You may not use this work except in compliance with the Licence. You may obtain a copy of the Licence at: http://ec.europa.eu/idabc/eupl.html   
 Unless required by applicable law or agreed to in writing, software distributed under the Licence is distributed on an “AS IS” basis, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the Licence permissions and limitations under the Licence.
---
+++ sctools/load_csv_points.py
@@ -0,0 +1,37 @@
+"""
+An IronPython script to be used in SpaceClaim to load a csv of points as
+spheres.
+
+It assumes that the first three columns of the .csv are X, Y, Z coordinates
+"""
+import csv
+
+# --- Input Parameters ---
+INFILE = r'path/to/file.csv'  # path to the .csv file
+RADIUS = MM(20)  # radius in mm of the spheres to be generated
+LIMITER = 1000  # max spheres to generate. Use None to have no limits
+SCALE = 1  # Scale the x,y,z coordinates by a factor
+
+
+# --- Functions ---
+def build_sphere(origin, radius, scale=1):
+    x = float(origin[0])*scale
+    y = float(origin[1])*scale
+    z = float(origin[2])*scale
+    start_point = Point.Create(x, y, z)
+    end_point = Point.Create(x, y, z+radius)
+    SphereBody.Create(start_point, end_point, ExtrudeType.ForceIndependent)
+
+
+# --- Code ---
+# reads input file lines and iterate on them
+with open(INFILE, 'r') as infile:
+    reader = csv.reader(infile, delimiter=',')
+    for i, line in enumerate(reader):
+        # skip the first line
+        if i == 0:
+            continue
+        elif LIMITER is not None and i > LIMITER:
+            break
+        # Build a sphere
+        build_sphere(line, RADIUS, scale=SCALE)
